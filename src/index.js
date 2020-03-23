261acffa1474ebd621ad6714c762d2e01de27c90
BarretteBanner <barrettebanner@gmail.com>
2020-03-23 15:26:31 -0500

    initialize

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const {ApolloServer, gql} = require('apollo-server')
+
+const typeDefs = gql``
+
+const resolvers = {}
+
+const server = new ApolloServer({typeDefs, resolvers})
+server.listen().then(({url}) => console.log(`server started at ${url}`))
