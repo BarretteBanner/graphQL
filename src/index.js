1c5ccc40ad257ce91cdcc33373637683c935a87f
BarretteBanner <barrettebanner@gmail.com>
2020-03-23 16:28:07 -0500

    added prettier

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const {ApolloServer, gql} = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server');
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('postgres://localhost:5432/barrettebanner')
+const sequelize = new Sequelize('postgres://localhost:5432/barrettebanner');
 
 const typeDefs = gql`
-type Query {
+  type Query {
     hello: String
-}
-`
+  }
+`;
 
-const resolvers = {}
+const resolvers = {};
 
-const server = new ApolloServer({typeDefs, resolvers})
-server.listen().then(({url}) => console.log(`server started at ${url}`))
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
 
 try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully!');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  sequelize.authenticate();
+  console.log('Connection has been established successfully!');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
