247b3acc5d3d770e9c81ce72e0dd9dc3fbd0dcaf
BarretteBanner <barrettebanner@gmail.com>
2020-03-23 15:58:35 -0500

    sequelize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const {ApolloServer, gql} = require('apollo-server')
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('postgres://localhost:5432/barrettebanner')
 
-const typeDefs = gql``
+const typeDefs = gql`
+type Query {
+    hello: String
+}
+`
 
 const resolvers = {}
 
 const server = new ApolloServer({typeDefs, resolvers})
 server.listen().then(({url}) => console.log(`server started at ${url}`))
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully!');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
