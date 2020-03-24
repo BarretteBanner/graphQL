c13485fe34c10064780c8f2624be33a8d1d3b9cb
BarretteBanner <52544117+BarretteBanner@users.noreply.github.com>
2020-03-24 09:17:11 -0500

    Barrette/connect (#1)
    * sequelize
    
    * added line to end of file
    
    * actaully did new line
    
    * added prettier
    
    * added dotenv
    
    * added line to end of file
    
    * improved code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-const {ApolloServer, gql} = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.POSTGRES_URL);
 
-const typeDefs = gql``
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-const resolvers = {}
+const resolvers = {};
 
-const server = new ApolloServer({typeDefs, resolvers})
-server.listen().then(({url}) => console.log(`server started at ${url}`))
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully!');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
