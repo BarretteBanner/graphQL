27edba05a78890b72729bb006563549e9a1dab7e
BarretteBanner <barrettebanner@gmail.com>
2020-03-26 12:10:15 -0500

    can create user from database and it fills in things like createdAt

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,11 +1,30 @@
-'use strict';
-const User = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    passward: DataTypes.STRING,
+module.exports = (sequelize, type) => {
+  return sequelize.define('user', {
+    id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: type.STRING,
+    username: {
+      type: type.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: type.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    createdAt: {
+      type: 'TIMESTAMP',
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: 'TIMESTAMP',
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
   });
-
-  return User;
 };
-
-module.exports = User;
